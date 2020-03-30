492214315497065c4ca433bc5fd5d01b04b9e6ae
julius <juliuszhang@outlook.com>
2020-03-30 16:59:50 +0800

    二叉树深度

---
+++ src/sword_offer/MaxDepth.java
@@ -0,0 +1,29 @@
+package sword_offer;
+
+import data.TreeNode;
+
+/**
+ * @author yibozhang
+ * @date 2020/3/30 16:56
+ * 输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+ *
+ * 例如：
+ *
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回它的最大深度 3 。
+ */
+public class MaxDepth {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+
+}
