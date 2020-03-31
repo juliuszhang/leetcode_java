e2b35dbb21c494b0b4d5209df82cc8206cdea807
julius <juliuszhang@outlook.com>
2020-03-31 12:58:13 +0800

    剑指offer

---
+++ src/sword_offer/NumWays.java
@@ -0,0 +1,33 @@
+package sword_offer;
+
+/**
+ * @author yibozhang
+ * @date 2020/3/31 11:07
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
+ * <p>
+ * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 2
+ * 输出：2
+ * 示例 2：
+ * <p>
+ * 输入：n = 7
+ * 输出：21
+ */
+public class NumWays {
+
+    public int numWays(int n) {
+        if (n <= 1) return 1;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+        }
+        return dp[n];
+    }
+
+}
