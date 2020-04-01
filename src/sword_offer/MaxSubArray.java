1aed70307c7cbc123ad68150ae2ffa32d4166723
julius <juliuszhang@outlook.com>
2020-04-01 22:26:58 +0800

    连续子数组的最大和

---
+++ src/sword_offer/MaxSubArray.java
@@ -0,0 +1,44 @@
+package sword_offer;
+
+/**
+ * @author yibozhang
+ * @date 2020/4/1 22:24
+ * 输入一个整型数组，数组里有正数也有负数。数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
+ *
+ * 要求时间复杂度为O(n)。
+ *
+ *  
+ *
+ * 示例1:
+ *
+ * 输入: nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= arr.length <= 10^5
+ * -100 <= arr[i] <= 100
+ *
+ */
+public class MaxSubArray {
+
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 0) return 0;
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = Math.max(nums[0], Integer.MIN_VALUE);
+        int max = dp[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            max = Math.max(dp[i], max);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        new MaxSubArray().maxSubArray(new int[]{1});
+    }
+
+}
